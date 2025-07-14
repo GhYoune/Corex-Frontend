fe9c131b75282b13f3339b9e4d17331a9c2b5901
gherbi younes <younes.gherbi@sigservice-dz.com>
2025-07-14 21:21:18 +0100

    🚸 progress bar for screens

---
+++ src/components/ProgressLinear.vue
@@ -0,0 +1,15 @@
+<template>
+ <v-progress-linear
+  color="primary"
+  :model-value="props.progress"
+  :height="8"
+  rounded></v-progress-linear>
+</template>
+
+<script setup lang="ts">
+ const props = defineProps<{
+  progress: number;
+ }>();
+</script>
+
+<style lang="scss"></style>
