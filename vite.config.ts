2e4a3b90e083805bebe1ce1356ca5fffec406301
GHERBI Younes <younes.gherbi@sigservice-dz.com>
2025-07-28 16:04:06 +0200

    🧑‍💻 🔧 [APP] @ alias for src added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import path from "path";
 import vuetify from "vite-plugin-vuetify";
 // https://vite.dev/config/
 export default defineConfig({
  plugins: [vue(), vuetify({ autoImport: true })],
+ resolve: {
+  alias: {
+   "@": path.resolve(__dirname, "./src"),
+  },
+ },
 });
